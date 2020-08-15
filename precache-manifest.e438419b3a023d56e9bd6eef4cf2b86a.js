9cd2d257d4f7629b910951eb7c0647aba2376f74
Ibán López <ibanlopezd@gmail.com>
2020-08-15 14:33:13 +0200

    Updates

---
+++ precache-manifest.e438419b3a023d56e9bd6eef4cf2b86a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0736fa70d703a4a5699ef85a6aead6da",
+    "url": "/inspiringquotes/index.html"
+  },
+  {
+    "revision": "b5d275b68d7569437a39",
+    "url": "/inspiringquotes/static/css/2.ae672d60.chunk.css"
+  },
+  {
+    "revision": "6177ffcfa43b66100e1c",
+    "url": "/inspiringquotes/static/css/main.4a7d8c04.chunk.css"
+  },
+  {
+    "revision": "b5d275b68d7569437a39",
+    "url": "/inspiringquotes/static/js/2.d0742ab1.chunk.js"
+  },
+  {
+    "revision": "e18637604789af6a40503615de5bd745",
+    "url": "/inspiringquotes/static/js/2.d0742ab1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6177ffcfa43b66100e1c",
+    "url": "/inspiringquotes/static/js/main.575e1b67.chunk.js"
+  },
+  {
+    "revision": "b9598e48bac9da638abb",
+    "url": "/inspiringquotes/static/js/runtime-main.b0a10666.js"
+  },
+  {
+    "revision": "05f1cdadfe476395f60e233b15c22155",
+    "url": "/inspiringquotes/static/media/icons-16.05f1cdad.eot"
+  },
+  {
+    "revision": "3c1c220e7a18286503fb431c7a7fe183",
+    "url": "/inspiringquotes/static/media/icons-16.3c1c220e.woff"
+  },
+  {
+    "revision": "3cde8748332d1de6b1ae1c2dc5850754",
+    "url": "/inspiringquotes/static/media/icons-16.3cde8748.ttf"
+  },
+  {
+    "revision": "0a5c76518a68c185baa2c6744456918c",
+    "url": "/inspiringquotes/static/media/icons-20.0a5c7651.eot"
+  },
+  {
+    "revision": "51ec31f302d0072808e1f83f85fea4cd",
+    "url": "/inspiringquotes/static/media/icons-20.51ec31f3.ttf"
+  },
+  {
+    "revision": "cef8cdbb9d0ba82e6e19fb0eeba2ac3d",
+    "url": "/inspiringquotes/static/media/icons-20.cef8cdbb.woff"
+  }
+]);
